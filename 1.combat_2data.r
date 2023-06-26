15faf975dbe4ad404fa0a0748d4945f324ba8726
Kun-Lin Ho <abc730814@gmail.com>
2023-06-26 16:39:15 -0400

    update combat2

--- 1.combat_2data.r
+++ 1.combat_2data.r
@@ -4,10 +4,12 @@ library(edgeR)
 library(limma)
 library(MultiAssayExperiment)
 library(sva)
-
+library(pvca)
 ## 06/26/23
 ## we can just use human TCGA data and ignore dogs
-load("1.combat_2data.rdata")
+load("E:/My Drive/Josh_MC_Paper_data/ML_gene_set/orig_results/1.combat_2data.rdata")
+source(
+  'C:/Users/abc73/Documents/GitHub/MC_subtyping/MC_subtyping_module.R')
 source(
   'C:/Users/abc73/Documents/GitHub/R_util/my_util.R')
 #'/Volumes/Research/GitHub/R_util/my_util.R')
@@ -16,43 +18,60 @@ source(
 base <- "E:/My Drive/Josh_MC_Paper_data/ML_gene_set"
 setwd(base)
 
-A.rna  <- read.csv("TCGA_BRCA_log2TPM+1.csv", row.names = 1, header = T)
+A.rna  <- fread("TCGA_BRCA_log2TPM+1.csv", header = T)
+colnames(A.rna)[1] <- 'GeneName'
 dim(A.rna) # 11856   664 samples
 
-B.rna  <- read.csv("cmt_all.csv", header = T)
+B.rna  <- fread("cmt_all.csv", header = T)
+### Mar-01 duplicated
+#B.rna <- B.rna[,-1]
+#setDF(B.rna)
 dim(B.rna) # 13220   144
-
-B.rna <- B.rna[ B.rna$GeneName %in% row.names(A.rna), ]
-B.rna <- B.rna[ match( row.names(A.rna),B.rna$GeneName ),]
+overlap_gene_list <- intersect(B.rna$GeneName,A.rna$GeneName)
+B.rna <- B.rna[GeneName %in% overlap_gene_list, ]
+A.rna <- A.rna[GeneName %in% overlap_gene_list, ]
+setDF(B.rna)
+setDF(A.rna)
+#B.rna <- na.omit(B.rna)
 row.names(B.rna) <- B.rna$GeneName
+row.names(A.rna) <- A.rna$GeneName
+A.rna <- A.rna[,-1]
 B.rna <- B.rna[,-1]
 abc = row.names(A.rna)== row.names(B.rna) #TRUE
-
 AB.rna <- cbind(A.rna,B.rna)
-phenotype_AB <- read.csv(file="phenotype_tcga_cmt.csv",header=T)
+
+phenotype_AB <- read.csv(file="All_dog_TCGA_subtype_meta.txt",header=T,sep="\t")
 AB.rna <- AB.rna[, colnames(AB.rna)%in% phenotype_AB$PATIENT_ID]
 #CMT has only simple cancer = 78 sam
+B_pheno <-phenotype_AB[phenotype_AB$SOURCE=='cmt',] 
 B.rna <- AB.rna[, colnames(AB.rna)%in% B_pheno$PATIENT_ID]
 
 phenotype_AB <- phenotype_AB[ match(colnames(AB.rna), phenotype_AB$PATIENT_ID ),]
 
-phenotype_AB<- phenotype_AB[,-1]
+#phenotype_AB<- phenotype_AB[,-1]
 #phenotype_AB$SOURCE <- as.factor(phenotype_AB$SOURCE)
 #phenotype_AB$LN_status <- as.factor(phenotype_AB$LN_status)
 #phenotype_AB$SUBTYPE <- as.factor(phenotype_AB$SUBTYPE)
 
-
-
 A_pheno <- phenotype_AB[phenotype_AB$SOURCE== "tcga",]
 B_pheno <- phenotype_AB[phenotype_AB$SOURCE== "cmt",]
 
-PCA_plot(t(AB.rna),phenotype_AB$SOURCE , title = "TCGA+CMT before combat")
-
-PCA_plot(t(AB.rna),phenotype_AB$SUBTYPE , title = "TCGA+CMT before combat")
-
-PCA_plot2(t(A.rna),A_pheno$SUBTYPE , title = "TCGA before combat")
-PCA_plot2(t(B.rna),B_pheno$SUBTYPE , title = "CMT before combat")
-
+pdf(file="pca_plot_source.pdf", width = 6,height = 4)
+p <- PCA_plot(t(AB.rna),phenotype_AB$SOURCE , title = "TCGA+CMT before combat")
+print(p)
+dev.off()
+pdf(file="pca_plot_sub.pdf", width = 6,height = 4)
+p <- PCA_plot(t(AB.rna),phenotype_AB$SUBTYPE , title = "TCGA+CMT before combat")
+print(p)
+dev.off()
+pdf(file="TCGA before combat.pdf", width = 6,height = 4)
+p <- PCA_plot2(t(A.rna),A_pheno$SUBTYPE , title = "TCGA before combat")
+print(p)
+dev.off()
+pdf(file="CMT before combat.pdf", width = 6,height = 4)
+p <- PCA_plot2(t(B.rna),B_pheno$SUBTYPE , title = "CMT before combat")
+print(p)
+dev.off()
 
 data <- data.frame(gene_id= rownames(AB.rna),AB.rna )
 
@@ -65,9 +84,10 @@ rownames(phenotype_AB) <- phenotype_AB$PATIENT_ID
 #transpose the data using t() to a wide format. We can also pass phenotype_AB[,-1], this
 #is necessary because the PCVA function looks for grouping variables to assess contribution
 #of variance so it is important not to pass any columns that contain unique identifiers.
-
-CBFpvcaFunction(t(data[,-1]), phenotypedata = phenotype_AB[,c(2,3,4)])
-
+pdf(file="CBFpvcaFunction.pdf", width = 6,height = 4)
+p <- CBFpvcaFunction(t(data[,-1]), phenotypedata = phenotype_AB[,c("SOURCE","SUBTYPE")])
+print(p)
+dev.off()
 
 
 # Here we build a model matrix with  covariates = phenotype_AB$CLASS
@@ -81,19 +101,29 @@ combat_batch_corrected = ComBat(dat = as.matrix(data[, -1]), batch = phenotype_A
 
 rownames(combat_batch_corrected) <- data$gene_id
 head(combat_batch_corrected)
+pdf(file="CBFpvcaFunction_combat_batch_corrected.pdf", width = 6,height = 4)
+p <- CBFpvcaFunction(t(combat_batch_corrected), phenotypedata = phenotype_AB[,c("SOURCE","SUBTYPE")])
+print(p)
+dev.off()
 
-CBFpvcaFunction(t(combat_batch_corrected), phenotypedata = phenotype_AB[,c(2,3,4)])
 
 #Plot PCA again after combat 
 data2  <- data.frame(t(combat_batch_corrected))
 
-PCA_plot(data = data2 , group = phenotype_AB$SOURCE , title = "TCGA+CMT after combat"  )
+pdf('TCGA_CMT after combat source.pdf', width = 6,height = 4)
+p <- PCA_plot(data = data2 , group = phenotype_AB$SOURCE , title = "TCGA+CMT after combat"  )
+print(p)
+dev.off()
 
-PCA_plot(data = data2 , group = phenotype_AB$SUBTYPE , title = "TCGA+CMT after combat"  )
-
-PCA_plot(data = data2 , group = phenotype_AB$SOURCE , 
+pdf('TCGA_CMT after combat subtype.pdf', width = 6,height = 4)
+p <- PCA_plot(data = data2 , group = phenotype_AB$SUBTYPE , title = "TCGA+CMT after combat"  )
+print(p)
+dev.off()
+pdf('TCGA_CMT after combat source_subtype.pdf', width = 6,height = 4)
+p <- PCA_plot(data = data2 , group = phenotype_AB$SOURCE , 
           label = phenotype_AB$SUBTYPE , title = "TCGA+CMT after combat"  )
-
+print(p)
+dev.off()
 
 tcga_corrected <- data.frame(combat_batch_corrected[,colnames(combat_batch_corrected)%in%A_pheno$PATIENT_ID ])
 dim(tcga_corrected) # 11856   664
@@ -101,15 +131,23 @@ dim(tcga_corrected) # 11856   664
 cmt_corrected <- data.frame(combat_batch_corrected[,colnames(combat_batch_corrected)%in%B_pheno$PATIENT_ID ])
 dim(cmt_corrected) #11856    78
 
-PCA_plot2(data = t(tcga_corrected) , group = A_pheno$SUBTYPE , title = "TCGA after combat"  )
-PCA_plot2(data = t(cmt_corrected) , group = B_pheno$SUBTYPE , title = "CMT after combat"  )
+pdf('TCGA after combat.pdf', width = 6,height = 4)
+p <- PCA_plot2(data = t(tcga_corrected) , group = A_pheno$SUBTYPE , title = "TCGA after combat"  )
+print(p)
+dev.off()
 
-write.csv(A_pheno,"phenotype_tcga.csv" )
-write.csv(B_pheno,"phenotype_cmt.csv" )
+pdf('CMT after combat.pdf', width = 6,height = 4)
+p <- PCA_plot2(data = t(cmt_corrected) , group = B_pheno$SUBTYPE , title = "CMT after combat"  )
+print(p)
+dev.off()
 
-write.csv(combat_batch_corrected,"AB_combat_corrected.csv")
-write.csv(tcga_corrected,"tcga_combat_corrected.csv")
-write.csv(cmt_corrected,"cmt_combat_corrected.csv")
 
-save.image("combat_2data.rdata")
+write.csv(A_pheno,"phenotype_all_tcga.csv" )
+write.csv(B_pheno,"phenotype_all_cmt.csv" )
+
+write.csv(combat_batch_corrected,"all_tcga_cmt_combat_corrected.csv")
+write.csv(tcga_corrected,"all_tcga_combat_corrected.csv")
+write.csv(cmt_corrected,"all_cmt_combat_corrected.csv")
+
+save.image("all_tcga_combat_2data.rdata")
 
---
+++ MC_subtyping_module.R
@@ -0,0 +1,98 @@
+PCA_plot <- function(data,group,label= "", title = "") {
+  Data_pca <- prcomp(scale(data,center = T,scale = T))
+  contributions <- summary(Data_pca)$importance[2,]*100
+  dataScores12 <-data.frame(Data_pca$x[, 1:2])
+  
+  if (label == "") {
+    p <- ggplot(dataScores12,aes(x=PC1,y=PC2))+
+      geom_point(aes(col=group),size=4)+ 
+      xlab(paste("PC1","(",round(contributions[1],digits=2),"%)",sep=""))+
+      ylab(paste("PC2","(",round(contributions[2],digits=2),"%)",sep=""))+
+      theme_bw(base_size = 16) + ggtitle(title)+
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  } else {
+    p <- ggplot(dataScores12,aes(x=PC1,y=PC2))+
+      geom_point(aes(col=group),size=4)+ geom_text(aes(label=label))+
+      xlab(paste("PC1","(",round(contributions[1],digits=2),"%)",sep=""))+
+      ylab(paste("PC2","(",round(contributions[2],digits=2),"%)",sep=""))+
+      theme_bw(base_size = 16)+ ggtitle(title)+
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  }
+  return(p)
+  
+}
+
+PCA_plot2 <- function(data,group,label= "", title = "") {
+  Data_pca <- prcomp(scale(data,center = T,scale = F))
+  contributions <- summary(Data_pca)$importance[2,]*100
+  dataScores12 <-data.frame(Data_pca$x[, 1:2])
+  
+  if (label == "") {
+    p <- ggplot(dataScores12,aes(x=PC1,y=PC2))+
+      geom_point(aes(col=group),size=4)+ 
+      xlab(paste("PC1","(",round(contributions[1],digits=2),"%)",sep=""))+
+      ylab(paste("PC2","(",round(contributions[2],digits=2),"%)",sep=""))+
+      theme_bw(base_size = 16) + ggtitle(title)+
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  } else {
+    p <- ggplot(dataScores12,aes(x=PC1,y=PC2))+
+      geom_point(aes(col=group),size=4)+ geom_text(aes(label=label))+
+      xlab(paste("PC1","(",round(contributions[1],digits=2),"%)",sep=""))+
+      ylab(paste("PC2","(",round(contributions[2],digits=2),"%)",sep=""))+
+      theme_bw(base_size = 16)+ ggtitle(title)+
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  }
+  
+}
+
+CBFpvcaFunction <- function(data,phenotypedata){
+  
+  #The only thing the function now assumes is that your phenotype table 
+  #is in the same order as your columns of your data.
+  
+  #packages <- c("pvca", "ggplot2", "Biobase") 
+  #lapply(packages, library, character.only = TRUE, quietly = TRUE)
+  
+  if(length(seq_len(nrow(phenotypedata))) != length(seq_len(nrow(data)))) {
+    warning(paste0("Data needs to be a wide", "\n", "Transposing data", "\n"))
+    data <- t(data)
+  }
+  
+  
+  if(!identical(rownames(phenotypedata), rownames(data))) {
+    warning(paste0("Rownames in phenotypedata do not match the rownames of data", "\n"))
+    warning(paste0("Matching rownames in phenotypedata to data", "\n"))
+    
+    if(nrow(phenotypedata) != nrow(data)) {
+      stop("Phenotypedata and data have different amounts of rows")
+    } else {
+      rownames(phenotypedata) <- rownames(data)
+    }
+  }
+  
+  data1_pheno_formatted<-new("AnnotatedDataFrame",data=phenotypedata)
+  
+  data_forPVCA<-ExpressionSet(assayData=t(data),phenoData=data1_pheno_formatted)
+  
+  pvcaObj_data <- pvcaBatchAssess (abatch = data_forPVCA, batch.factors = colnames(data1_pheno_formatted@data), threshold = 0.7)
+  
+  data_pvca_res<-data.frame(as.data.frame(pvcaObj_data$label),t(as.data.frame(pvcaObj_data$dat)))
+  
+  colnames(data_pvca_res)<-c("effect","variance")
+  
+  
+  p<-ggplot((data_pvca_res[-nrow(data_pvca_res),]), aes(x= effect, y = variance)) + 
+    geom_bar(stat = 'identity',  position = 'dodge', col ='transparent')+ 
+    scale_fill_discrete(guide = 'none') + 
+    theme_bw(base_size = 16)+
+    theme(plot.title =  element_text(hjust=0.5),axis.text.x=element_text(angle=45,hjust=1))+
+    labs(x = 'Effects', y = 'Weighted average proportion variance')+
+    ggtitle("PVCA estimation bar chart corrected data")
+  
+  return(p)
+  
+}
